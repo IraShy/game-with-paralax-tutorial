e69b6bb22b292e7facdebbe4ee11269b414bf9b1
irashy <i.shymbor@gmail.com>
2023-04-24 15:22:56 +1000

    Project 3: second movement pattern

--- Proj3-enemy-movement-pattern-2/script.js
+++ Proj3-enemy-movement-pattern-2/script.js
@@ -4,7 +4,7 @@ const ctx = canvas.getContext("2d");
 // the following 2 values must be the same as given in the css
 CANVAS_WIDTH = canvas.width = 500;
 CANVAS_HEIGHT = canvas.height = 1000;
-const numberOfEnemies = 10;
+const numberOfEnemies = 20;
 const enemiesArray = [];
 
 let gameFrame = 0;
@@ -12,10 +12,10 @@ let gameFrame = 0;
 class Enemy {
   constructor() {
     this.image = new Image();
-    this.image.src = "./enemies/enemy1.png";
-    // this.speed = Math.random() * 4 - 2;
-    this.spriteWidth = 293; // the width of a single frame in pixels
-    this.spriteHeight = 155;
+    this.image.src = "./enemies/enemy2.png";
+    this.speed = Math.random() * 4 + 1;
+    this.spriteWidth = 266; // the width of a single frame in pixels
+    this.spriteHeight = 188;
     this.height = this.spriteHeight / 2.5;
     this.width = this.spriteWidth / 2.5;
     this.x = Math.random() * (canvas.width - this.width);
@@ -23,12 +23,19 @@ class Enemy {
     // there's a proper way to time animation frames using request animation frame and delta time, but in this tutorial we'll slow down our animation in the simplest way possible.
     this.frame = 0;
     this.flapSpeed = Math.floor(Math.random() * 3 + 1);
+    // this.angle = Math.random() * 4;
+    this.angle = 0;
+    this.angleSpeed = Math.random() * 0.2;
+    this.curve = Math.random() * 7;
   }
   update() {
-    // this.x += this.speed;
+    this.x -= this.speed;
+    this.y += Math.sin(this.angle) * this.curve;
+    this.angle += this.angleSpeed;
     // this.y += this.speed;
-    this.x += Math.random() * 5 - 2.5;
-    this.y += Math.random() * 5 - 2.5;
+    // this.x += Math.random() * 5 - 2.5;
+    // this.y += Math.random() * 5 - 2.5;
+    if (this.x + this.width < 0) this.x = canvas.width;
     if (gameFrame % this.flapSpeed === 0) {
       this.frame > 4 ? (this.frame = 0) : this.frame++;
     }
@@ -64,5 +71,3 @@ function animate() {
   requestAnimationFrame(animate);
 }
 animate();
-
-// 1:36:07
