f3dbeb07517a93aca14afe69fdb422221fd92efb
irashy <i.shymbor@gmail.com>
2023-05-16 22:11:35 +1000

    Project 4: collision animations with sound

--- Proj4-collision-animations/script.js
+++ Proj4-collision-animations/script.js
@@ -18,14 +18,21 @@ class Explosion {
     this.width = this.spriteWidth * 0.5;
     this.height = this.spriteHeight * 0.5;
     // offset the x and y by a half of the image size to draw the explosion with the centre in the point of click. We can do the offsetting either here or in the draw method. We cannot do it in the addEventListener because at that point we don't have the image size, x, and y
-    this.x = x - this.width / 2;
-    this.y = y - this.height / 2;
+    // this.x = x - this.width / 2;
+    // this.y = y - this.height / 2;
+
+    this.x = x;
+    this.y = y;
     this.image = new Image();
     this.image.src = "boom.png";
     this.frame = 0;
     this.timer = 0; // to slow the animation and ensure that all frames of the image are drawn (the initial is not drawn at normal speed because the increment happens before???)
+    this.angle = Math.random() * 6.2; // rotate function takes an angle in radians, and 6.2 is roughly 360 degrees in rads.
+    this.sound = new Audio();
+    this.sound.src = "boom.wav";
   }
   update() {
+    if (this.frame === 0) this.sound.play();
     this.timer++;
     if (this.timer % 10 === 0) {
       // this makes it 10 times slower: the code runs every 10 frames
@@ -33,33 +40,40 @@ class Explosion {
     }
   }
   draw() {
+    // this is how you rotate things on canvas: you save the current state on canvas to make sure the following changes affect only 1 draw call
+
+    ctx.save();
+    ctx.translate(this.x, this.y);
+    ctx.rotate(this.angle);
     ctx.drawImage(
       this.image,
       this.spriteWidth * this.frame,
       0,
       this.spriteWidth,
       this.spriteHeight,
-      this.x,
-      this.y,
+      // this.x,
+      // this.y,
+      // replacing the coordinates with 0 because we already have the this.x and this.y in ctx.translate; we will also offset it by the half-width and half-height of the sprite
+      0 - this.width / 2,
+      0 - this.height / 2,
       this.width,
       this.height
     );
+    ctx.restore();
   }
 }
 window.addEventListener("click", function (e) {
+  createAnimation(e);
+});
+// window.addEventListener("mousemove", function (e) {
+//   createAnimation(e);
+// });
+
+function createAnimation(e) {
   let positionX = e.x - canvasPosition.left;
   let positionY = e.y - canvasPosition.top;
-  // practicing: draw white rectangles in the point of click
-  // ctx.fillStyle = "white";
-  // // ctx.fillRect(e.x, e.y, 50, 50); // this will result in rectangles being drawn, but not exactly under the mouse: it will be drawn much below. That's because the canvas is positioned in the middle of the page rather than at its top, so we need to offset the canvas position.
-  // ctx.fillRect(
-  //   positionX - 25, // 25 is the half of the width of 50 below: to draw the rectangle with the point of click being in its centre
-  //   positionY - 25,
-  //   50,
-  //   50
-  // );
   explosions.push(new Explosion(positionX, positionY));
-});
+}
 
 // animation loop
 function animate() {
@@ -77,4 +91,4 @@ function animate() {
 }
 animate();
 
-// 2:43:25
+// 2:47:50
